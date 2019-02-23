48e2a28cbdba9a216a0c368154cfd8d08b849fec
orfeas <erevos13@gmail.com>
2019-02-24 01:44:32 +0200

    add metadata

--- server/boot/userRemoteMethods.js
+++ server/boot/userRemoteMethods.js
@@ -1,5 +1,6 @@
 module.exports = function (app) {
   const User = app.models.User;
+  const Metadata = app.models.metadata;
 
   User.balances = function (msg, cb) {
     console.log(msg);
@@ -8,24 +9,35 @@ module.exports = function (app) {
     cb(null, 'Greetings... ' - msg);
   };
 
+  User.getUserProfile = function(userId, cb) {
+    console.log(userId);
+    Metadata.find({fields: {itemId: userId, metadata: true}},function (err, metadata) {
+      console.log({metadataUser: metadata});
+      cb(null, metadata);
+    });
+  };
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  User.remoteMethod(
+    'getUserProfile', {
+      accepts: {
+        arg: 'userId',
+        type: 'string',
+        required: true,
+      },
+      returns: {
+        arg: 'userDTO',
+        type: 'object',
+      },
+      http: {
+        path: '/getUserProfile',
+        verb: 'get',
+      }
+    }
+  );  /**
+   * @description get the user balances
+   */
   User.remoteMethod(
     'balances', {
       accepts: {
