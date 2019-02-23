475c62b042d88dc7b337764610ebee7b8fd5279d
orfeas <erevos13@gmail.com>
2019-02-23 11:03:16 +0200

    add balances

---
+++ server/boot/userRemoteMethods.js
@@ -0,0 +1,26 @@
+module.exports = function (app) {
+  const User = app.models.User;
+
+  User.balances = function (msg, cb) {
+    console.log(msg);
+    cb(null, 'Greetings... ' - msg);
+  };
+
+  User.remoteMethod(
+    'balances', {
+      accepts: {
+        arg: 'userId',
+        type: 'string',
+        required: true,
+      },
+      returns: {
+        arg: 'userBalances',
+        type: 'object',
+      },
+      http: {
+        path: '/balances',
+        verb: 'get',
+      },
+    }
+  );
+};
